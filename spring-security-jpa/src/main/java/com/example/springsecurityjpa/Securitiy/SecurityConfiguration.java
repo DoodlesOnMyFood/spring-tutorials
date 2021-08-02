ca22ed7b54e3bfdcac537679c139e443a9e7551c
Jisoo Choi <fyimbtmn@gmail.com>
2021-08-02 19:25:17 +0900

    Fixed config

--- spring-security-jpa/src/main/java/com/example/springsecurityjpa/Securitiy/SecurityConfiguration.java
+++ spring-security-jpa/src/main/java/com/example/springsecurityjpa/Securitiy/SecurityConfiguration.java
@@ -1,5 +1,6 @@
 package com.example.springsecurityjpa.Securitiy;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
@@ -13,11 +14,13 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 @Configuration
 @EnableWebSecurity
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
-    UserDetailsService userDetailsService;
+    @Autowired
+    private MyUserDetailService myUserDetailService;
+
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        super.configure(auth);
+        auth.userDetailsService(myUserDetailService);
     }
 
     @Override
